41787a554546ebcb94243c91d27a718118869298
Steven Nguyen <149844435+stevespeace@users.noreply.github.com>
2025-10-28 18:21:17 +0000

    a

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main
 {
   public static void main(String[] args)
   {
-    printNums(); 
+    uprightNumberTriangle(50);
   }
 
   public static void printNTimes(String word, int N)
@@ -18,14 +18,26 @@ public class Main
     {
       for(int j = 1; j <= i; j++)
       {
-        System.out.println(i);
+        System.out.print(i +" ");
       }
+      System.out.println(" ");
     }
   }
 
   public static void uprightNumberTriangle(int N)
   {
-    // code solution here
+    for(int i = 1; i <= N; i++)
+    {
+      for(int j = 1; j <= i ; j++)
+      {
+        System.out.print(j +" ");
+      }
+      System.out.println(" ");
+    }
+
+
+
+
   }
 
   public static void starTree()
